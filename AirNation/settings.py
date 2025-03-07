13465ed3cb1342d2b60739e85f05b38071cd13d6
Amani Daudi <mtuwamungu01@gmail.com>
2025-03-06 19:01:45 -0800

    Enable DEBUG mode to show errors in the browser

--- AirNation/settings.py
+++ AirNation/settings.py
@@ -51,7 +51,7 @@ env = environ.Env(
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 SECRET_KEY = env("SECRET_KEY")
-DEBUG = env("DEBUG")
+DEBUG = env.bool('DEBUG', default=True)
 
 # Allowed Hosts & CSRF Trusted Origins
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
